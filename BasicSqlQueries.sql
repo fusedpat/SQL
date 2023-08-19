e382a71020c2d9b6ed81be6d685e2ff21fc20f93
fusedpat <58187861+fusedpat@users.noreply.github.com>
2023-08-19 12:37:42 -0400

    Add files via upload

---
+++ BasicSqlQueries.sql
@@ -0,0 +1,134 @@
+SELECT * FROM Customers;
+SELECT CustomerName, City FROM Customers;
+SELECT Country FROM Customers;
+SELECT DISTINCT Country FROM Customers;
+SELECT * FROM Customers WHERE Country='Mexico';
+SELECT * FROM Customers WHERE CustomerID=1;
+SELECT * FROM Products WHERE Price = 18;
+SELECT * FROM Products WHERE Price > 30;
+SELECT * FROM Products WHERE Price < 30;
+SELECT * FROM Products WHERE Price >= 30;
+SELECT * FROM Products WHERE Price <= 30;
+SELECT * FROM Products WHERE Price <> 18;--Some servers has different like !=
+SELECT * FROM Products WHERE Price BETWEEN 50 AND 60;
+SELECT * FROM Customers WHERE City LIKE 's%';
+SELECT * FROM products WHERE productname LIKE '%Bob''s%';
+SELECT * FROM Customers WHERE City IN ('Paris','London');
+SELECT * FROM Customers WHERE Country='Germany' AND City='Berlin';
+SELECT * FROM Customers WHERE City='Berlin' OR City='München';
+SELECT * FROM Customers WHERE Country='Germany' OR Country='Spain';
+SELECT * FROM Customers WHERE NOT Country='Germany';
+SELECT * FROM Customers WHERE Country='Germany' AND (City='Berlin' OR City='München');
+SELECT * FROM Customers WHERE NOT Country='Germany' AND NOT Country='USA';
+
+SELECT * FROM Customers
+WHERE Country IN ('Germany', 'France', 'UK');
+
+SELECT * FROM Customers
+WHERE Country NOT IN ('Germany', 'France', 'UK');
+
+SELECT * FROM Customers
+WHERE Country IN (SELECT Country FROM Suppliers);
+
+
+SELECT * FROM Products
+WHERE Price BETWEEN 10 AND 20;
+
+SELECT * FROM Products
+WHERE Price NOT BETWEEN 10 AND 20;
+
+SELECT * FROM Products
+WHERE Price BETWEEN 10 AND 20
+AND CategoryID NOT IN (1,2,3)
+
+
+SELECT * FROM Products
+WHERE ProductName BETWEEN 'Carnarvon Tigers' AND 'Mozzarella di Giovanni'
+ORDER BY ProductName;
+
+SELECT * FROM Products
+WHERE ProductName BETWEEN "Carnarvon Tigers" AND "Chef Anton's Cajun Seasoning"
+ORDER BY ProductName;
+
+SELECT * FROM Products
+WHERE ProductName NOT BETWEEN 'Carnarvon Tigers' AND 'Mozzarella di Giovanni'
+ORDER BY ProductName;
+
+SELECT * FROM Customers ORDER BY Country;
+SELECT * FROM Customers ORDER BY Country DESC;
+SELECT * FROM Customers ORDER BY Country, CustomerName;
+SELECT * FROM Customers ORDER BY Country ASC, CustomerName DESC;
+SELECT CustomerName, ContactName, Address FROM Customers WHERE Address IS NULL;
+SELECT CustomerName, ContactName, Address FROM Customers WHERE Address IS NOT NULL;
+
+SELECT MIN(Price) AS SmallestPrice FROM Products;
+
+SELECT MAX(Price) AS LargestPrice FROM Products;
+
+SELECT COUNT(ProductID) FROM Products;
+
+SELECT AVG(Price) FROM Products;
+
+SELECT SUM(Quantity) FROM OrderDetails;
+
+
+-- Like Refer to https://www.w3schools.com/sql/sql_like.asp  -------
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'a%';
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE '%a';
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE '%or%';
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE '_r%';
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'a__%';
+
+SELECT * FROM Customers
+WHERE ContactName LIKE 'a%o';
+
+SELECT * FROM Customers
+WHERE CustomerName NOT LIKE 'a%';
+
+-- Wild cards https://www.w3schools.com/sql/sql_wildcards.asp ----
+
+SELECT * FROM Customers
+WHERE City LIKE 'ber%';
+
+
+SELECT * FROM Customers
+WHERE City LIKE '%es%';
+
+SELECT * FROM Customers
+WHERE City LIKE '_ondon';
+
+SELECT * FROM Customers
+WHERE City LIKE 'L_n_on';
+
+SELECT * FROM Customers
+WHERE City LIKE '[bsp]%';
+
+SELECT * FROM Customers
+WHERE City LIKE '[a-c]%';
+
+-- TOP -------
+--Mysql
+SELECT * FROM Customers LIMIT 3;
+
+SELECT TOP 3 * FROM Customers
+WHERE Country='Germany';
+
+
+-- Oracle
+SELECT * FROM Customers FETCH FIRST 3 ROWS ONLY;
+
+SELECT * FROM Customers
+WHERE Country='Germany'
+FETCH FIRST 3 ROWS ONLY;
+
+
